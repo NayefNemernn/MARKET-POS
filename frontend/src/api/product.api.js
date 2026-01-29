91de3af62cc88444aacb85fe8a9bcdbe40937ae2
NayefNemernn <nayefnemernn@gmail.com>
2026-01-29 03:31:38 +0200

    add products

--- frontend/src/api/product.api.js
+++ frontend/src/api/product.api.js
@@ -1,5 +1,9 @@
 import api from "./axios";
 
+/* =======================
+   CASHIER / POS
+======================= */
+
 export const getProductByBarcode = async (barcode) => {
   const res = await api.get(`/products/barcode/${barcode}`);
   return res.data;
@@ -8,4 +12,22 @@ export const getProductByBarcode = async (barcode) => {
 export const getAllProducts = async () => {
   const res = await api.get("/products");
   return res.data;
+};
+
+/* =======================
+   ADMIN
+======================= */
+
+export const createProduct = async (data) => {
+  const res = await api.post("/products", data);
+  return res.data;
+};
+
+export const updateProduct = async (id, data) => {
+  const res = await api.put(`/products/${id}`, data);
+  return res.data;
+};
+
+export const deleteProduct = async (id) => {
+  await api.delete(`/products/${id}`);
 };
