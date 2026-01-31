87da0ef459a187c424253339cffdc7cdc54c4a11
NayefNemernn <nayefnemernn@gmail.com>
2026-01-31 16:55:53 +0200

    update user

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,20 +7,31 @@ import productRoutes from "./routes/product.routes.js";
 import saleRoutes from "./routes/sale.routes.js";
 import categoryRoutes from "./routes/category.routes.js";
 import dashboardRoutes from "./routes/dashboard.routes.js";
+import userRoutes from "./routes/user.routes.js";
 
 dotenv.config();
 
 const app = express();
 
+/* =======================
+   MIDDLEWARE
+======================= */
 app.use(cors());
 app.use(express.json());
 
+/* =======================
+   ROUTES
+======================= */
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/sales", saleRoutes);
 app.use("/api/categories", categoryRoutes);
 app.use("/api/dashboard", dashboardRoutes);
+app.use("/api/users", userRoutes);
 
+/* =======================
+   HEALTH CHECK
+======================= */
 app.get("/", (req, res) => {
   res.send("Market POS API running");
 });
