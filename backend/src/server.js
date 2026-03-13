87d2e18247de5e84e0d6607f60d499e3eb80713c
NayefNemernn <nayefnemernn@gmail.com>
2026-03-13 23:10:29 +0200

    test

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import app from "./app.js";
 import { connectDB } from "./config/db.js";
 import { PORT } from "./config/env.js";
+import { fileURLToPath } from "url";
 
 import express from "express";
 import path from "path";
@@ -8,11 +9,16 @@ import path from "path";
 const startServer = async () => {
 
   await connectDB();
+  const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
   const port = process.env.PORT || PORT || 5000;
 
   /* SERVE PRODUCT IMAGES */
-  app.use("/uploads", express.static(path.join(process.cwd(),"backend/uploads")));
+  app.use(
+  "/uploads",
+  express.static(path.join(__dirname, "../../uploads"))
+);
 
   app.listen(port, () => {
     console.log(`🚀 Server running on port ${port}`);
